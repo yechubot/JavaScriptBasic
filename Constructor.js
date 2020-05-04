8bba20de04274fc7944eab09e8f303b65645fb5b
yechubot <61838900+yechubot@users.noreply.github.com>
2020-05-04 13:34:46 +0900

    Create Constructor.js

---
+++ Constructor.js
@@ -0,0 +1,20 @@
+// 변수 대문잘 시작해서 만듦 - > Consturctor
+var Car = function(maxSpeed, driver) {
+  
+  this.maxSpeed = maxSpeed;
+  this.driver = driver;
+  
+  this.drive = function(speed, time) {
+  console.log(speed * time );
+};
+this.logDrive = function() {
+console.log("driver name is " + this.driver);
+};
+
+}
+
+var myCar = new Car(70, "bot");
+var myCar2 = new Car(30, "yechu");
+var myCar3 = new Car(50, "max");
+var myCar4 = new Car(20, "robot");
+
