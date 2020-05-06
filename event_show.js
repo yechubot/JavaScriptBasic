0624ed427ed1d4be3c438a785175982b73380dce
yechubot <61838900+yechubot@users.noreply.github.com>
2020-05-06 13:10:48 +0900

    Update event_show.js

--- event_show.js
+++ event_show.js
@@ -21,3 +21,27 @@ function loadJs() {
 window.onload = function(){
      loadJs();
 };
+/* same with below ( below is not organized )
+
+window.onload = function(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+
+    button.onclick = function() {
+
+    if(content.className == "open"){
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Show more";
+    }else {
+        //expand the box
+        content.className = "open";
+        button.innerHTML = "Show less";
+    };
+
+}
+
+};
+
+
+*/
